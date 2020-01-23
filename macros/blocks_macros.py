d7b7b8182be50145f30964de1195ae900fa8d43d
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-01-23 16:44:12 +0100

    FIX: Macro

--- macros/blocks_macros.py
+++ macros/blocks_macros.py
@@ -3,9 +3,11 @@
     blocks used in the docs of Kodular.
 """
 
-import mdx_macros
-import urllib.parse
 import json
+import urllib.parse
+
+import mdx_macros
+
 
 class PropertyBlockSetterMacro(mdx_macros.BaseMacro):
     """
@@ -15,16 +17,16 @@ class PropertyBlockSetterMacro(mdx_macros.BaseMacro):
 
     """
     name = 'Setter Property Block macro'
-    key  = 'PropertyBlockSetter'
+    key = 'PropertyBlockSetter'
 
     def handler(self, component_name, property_name, *args, **kwargs):
-
         div = '<div class="block" ai2-block="property" not-rendered="true" value="{value}"></div>'
-        value_dict = {"componentName": component_name,  "name": property_name, "getter": False}
+        value_dict = {"componentName": component_name, "name": property_name, "getter": False}
         value_str = json.dumps(value_dict)
-        quoted = urllib.parse.quote(value_str, safe='~@#$&()*!+=:;,.?/\''); # equivalent of encodeURI in JS
+        quoted = urllib.parse.quote(value_str, safe='~@#$&()*!+=:;,.?/\'')  # equivalent of encodeURI in JS
 
         return div.format(value=quoted)
+
 
 class PropertyBlockGetterMacro(mdx_macros.BaseMacro):
     """
@@ -34,16 +36,16 @@ class PropertyBlockGetterMacro(mdx_macros.BaseMacro):
     """
 
     name = 'Getter Property Block macro'
-    key  = 'PropertyBlockGetter'
+    key = 'PropertyBlockGetter'
 
     def handler(self, component_name, property_name, *args, **kwargs):
-
         div = '<div class="block" ai2-block="property" not-rendered="true" value="{value}"></div>'
-        value_dict = {"componentName": component_name,  "name": property_name, "getter": True}
+        value_dict = {"componentName": component_name, "name": property_name, "getter": True}
         value_str = json.dumps(value_dict)
-        quoted = urllib.parse.quote(value_str, safe='~@#$&()*!+=:;,.?/\''); # equivalent of encodeURI in JS
+        quoted = urllib.parse.quote(value_str, safe='~@#$&()*!+=:;,.?/\'')  # equivalent of encodeURI in JS
 
         return div.format(value=quoted)
+
 
 class PropertyBlockGetAndSetMacro(mdx_macros.BaseMacro):
     """
@@ -53,17 +55,17 @@ class PropertyBlockGetAndSetMacro(mdx_macros.BaseMacro):
 
     """
     name = 'Property Block Shorthand macro'
-    key  = 'PropertyBlockGetterAndSetter'
+    key = 'PropertyBlockGetterAndSetter'
 
     def handler(self, component_name, property_name, *args, **kwargs):
-        pbgm = PropertyBlockGetterMacro(False, {}) # Dummy Macro
-        pbsm = PropertyBlockSetterMacro(False, {}) # Dummy Macro
+        pbgm = PropertyBlockGetterMacro(False, {})  # Dummy Macro
+        pbsm = PropertyBlockSetterMacro(False, {})  # Dummy Macro
         getter = pbgm.handler(component_name, property_name)
         setter = pbsm.handler(component_name, property_name)
 
-        # Extra <div> has been added to maintain a correct XML structure, because the markdown-macros parses the returned
-        # string as XML
-        return '<div>' + setter + '\n'+ getter + '</div>'
+        # Extra <div> has been added to maintain a correct XML structure, because the markdown-macros parses the
+        # returned string as XML
+        return '<div>' + setter + '\n' + getter + '</div>'
 
 
 class EventBlockMacro(mdx_macros.BaseMacro):
@@ -76,15 +78,14 @@ class EventBlockMacro(mdx_macros.BaseMacro):
     """
 
     name = 'Event Block macro'
-    key  = 'Event'
+    key = 'Event'
 
     def handler(self, component_name, event_name, *params, **kwargs):
-
         div = '<div class="block" ai2-block="event" not-rendered="true" value="{value}"></div>'
-        value_dict = {"componentName": component_name,  "name": event_name, "param": params}
+        value_dict = {"componentName": component_name, "name": event_name, "param": params}
 
         value_str = json.dumps(value_dict)
-        quoted = urllib.parse.quote(value_str, safe='~@#$&()*!+=:;,.?/\''); # equivalent of encodeURI in JS
+        quoted = urllib.parse.quote(value_str, safe='~@#$&()*!+=:;,.?/\'')  # equivalent of encodeURI in JS
 
         return div.format(value=quoted)
 
@@ -97,20 +98,21 @@ class MethodBlockMacro(mdx_macros.BaseMacro):
 
         Add the parameters of a method to the end of the macro call.
         - `[[Method('Button', 'Simulate Click', False)]]` - for an method with no return value and without parameters.
-        - `[[Method('Device Utility', 'Copy', False, 'success Toast Message')]]` - for a  method with no return value and with parameters.
-        - `[[Method('Color Utility', 'Get Luminance', True,  'color')]]` - for a method with return value and with parameters.
+        - `[[Method('Device Utility', 'Copy', False, 'success Toast Message')]]` - for a  method with no return value
+            and with parameters.
+        - `[[Method('Color Utility', 'Get Luminance', True,  'color')]]` - for a method with return value and with
+            parameters.
         - `[[Method('Device Utility', 'Is Emulator', True)]]` - for a method with return value and without parameters.
 
     """
 
     name = 'Method Block macro'
-    key  = 'Method'
+    key = 'Method'
 
-    def handler(self, component_name, method_name, has_return_value=False,*params, **kwargs):
-
+    def handler(self, component_name, method_name, has_return_value=False, *params, **kwargs):
         div = '<div class="block" ai2-block="method" not-rendered="true" value="{value}"></div>'
-        value_dict = {"componentName": component_name,  "name": method_name, "param": params, "output": has_return_value}
+        value_dict = {"componentName": component_name, "name": method_name, "param": params, "output": has_return_value}
         value_str = json.dumps(value_dict)
-        quoted = urllib.parse.quote(value_str, safe='~@#$&()*!+=:;,.?/\''); # equivalent of encodeURI in JS
+        quoted = urllib.parse.quote(value_str, safe='~@#$&()*!+=:;,.?/\'')  # equivalent of encodeURI in JS
 
         return div.format(value=quoted)
