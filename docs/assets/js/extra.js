6c2124e3cfa852c3e9866e4939eecff1efdbb5b6
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-01-27 16:53:24 +0100

    REV

--- docs/assets/js/extra.js
+++ docs/assets/js/extra.js
@@ -9,11 +9,6 @@ $(document).ready(function () {
     // Lightbox
     // event.preventDefault();
     // $(this).ekkoLightbox();
-
-
-    $('.contributors img[data-src]').each(function () {
-        $(this).attr('src', $(this).attr("data-src"));
-    });
 });
 
 
